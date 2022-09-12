51292091cf83838e2bdf5680c277319dfb1c4249
selvan123 <selvans@gmail.com>
2022-09-12 16:01:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,34 +6,35 @@ terraform {
     key                  = "terraformgithubexample.tfstate"
   }
 }
- 
-provider "azurerm" {
-  # The "feature" block is required for AzureRM provider 2.x.
-  # If you're using version 1.x, the "features" block is not allowed.
-  version = "~>2.0"
-  features {}
+   
+provider "azurerm" {    
+  features {}    
 }
- 
-data "azurerm_client_config" "current" {}
- 
-#Create Resource Group
-resource "azurerm_resource_group" "tamops" {
-  name     = "tamops"
-  location = "eastus2"
+
+resource "azurerm_resource_group" "resource_group" {
+  name     = "app-service-rg"
+  location = "East US"
 }
- 
-#Create Virtual Network
-resource "azurerm_virtual_network" "vnet" {
-  name                = "tamops-vnet"
-  address_space       = ["192.168.0.0/16"]
-  location            = "eastus2"
-  resource_group_name = azurerm_resource_group.tamops.name
+
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name                = "demoappservice-plan"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  kind                = "Windows"
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
 }
- 
-# Create Subnet
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet"
-  resource_group_name  = azurerm_resource_group.tamops.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix       = "192.168.0.0/24"
+
+resource "azurerm_app_service" "app_service" {
+  name                = "DotNetCoreDemoApp"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+
+  site_config {
+    windows_fx_version = "DOTNETCORE|3.1"
+  }
+
 }
