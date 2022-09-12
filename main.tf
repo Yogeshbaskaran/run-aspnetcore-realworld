bb1a1bd3f9431cc35bb09bb11b5856a178405a70
selvan123 <selvans@gmail.com>
2022-09-12 15:54:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,39 +1,39 @@
-terraform {    
-  required_providers {    
-    azurerm = {    
-      source = "hashicorp/azurerm"    
-    }    
-  }    
-} 
-   
-provider "azurerm" {    
-  features {}    
-}
-
-resource "azurerm_resource_group" "resource_group" {
-  name     = "app-service-rg"
-  location = "East US"
-}
-
-resource "azurerm_app_service_plan" "app_service_plan" {
-  name                = "demoappservice-plan"
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
-  kind                = "Windows"
-  sku {
-    tier = "Basic"
-    size = "B1"
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tamopstfstates"
+    storage_account_name = "tfstatedevops"
+    container_name       = "terraformgithubexample"
+    key                  = "terraformgithubexample.tfstate"
   }
 }
-
-resource "azurerm_app_service" "app_service" {
-  name                = "DotNetCoreDemoApp"
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
-  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
-
-  site_config {
-    windows_fx_version = "DOTNETCORE|3.1"
-  }
-
+ 
+provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x.
+  # If you're using version 1.x, the "features" block is not allowed.
+  version = "~>2.0"
+  features {}
+}
+ 
+data "azurerm_client_config" "current" {}
+ 
+#Create Resource Group
+resource "azurerm_resource_group" "tamops" {
+  name     = "tamops"
+  location = "eastus2"
+}
+ 
+#Create Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "tamops-vnet"
+  address_space       = ["192.168.0.0/16"]
+  location            = "eastus2"
+  resource_group_name = azurerm_resource_group.tamops.name
+}
+ 
+# Create Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.tamops.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = "192.168.0.0/24"
 }
