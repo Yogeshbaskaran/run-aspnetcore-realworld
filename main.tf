37ea6a808f191b58048394c2387fe09245e48571
selvan123 <selvans@gmail.com>
2022-09-12 16:08:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tamopstfstates"
-    storage_account_name = "tfstatedevops"
-    container_name       = "terraformgithubexample"
-    key                  = "terraformgithubexample.tfstate"
-  }
-}
+terraform {    
+  required_providers {    
+    azurerm = {    
+      source = "hashicorp/azurerm"    
+    }    
+  }    
+} 
    
 provider "azurerm" {    
   features {}    
